3fd8813ca791fbc7de7c447bfd12c9a195935606
Raffael Bottoli Schemmer <raffael.schemmer@acad.pucrs.br>
2017-06-04 22:49:09 -0300

    Create Cache.h

---
+++ Cache.h
@@ -0,0 +1,12 @@
+#ifndef Cache_h 
+#define Cache_h
+void iniciar_simulador();
+void mostrar_memorias();
+void mostrar_estatisticas();
+void mostrar_estatisticas();
+void prenche_endereco(int,char[]);
+void divide_endereco(int,int *,int *);
+int procura_quadro_menor_acesso();
+int le_endereco(int,int *,int *,int *,int *);
+int escreve_endereco(int,int *,int *,int *,int *);
+#endif
